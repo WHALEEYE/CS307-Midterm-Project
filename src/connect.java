8ce07297ea4b4d1bf6095c1898e86deaf0ae1b39
WHALEEYE <1094349973@qq.com>
2021-03-30 22:20:41 +0800

    temp
    temp sync

--- src/connect.java
+++ src/connect.java
@@ -4,8 +4,11 @@ import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonSyntaxException;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -15,6 +18,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class connect {
+
   public static void main(String[] args) throws Exception {
     // 参数：
     // jdbc协议:postgresql子协议://主机地址:数据库端口号/要连接的数据库名
@@ -31,7 +35,7 @@ public class connect {
 
     try {
       //新建解析json文件的模块
-      String dataPath = "F:\\数据库原理\\project\\project1\\src\\course_info.json";//配置解析的文本路径
+      String dataPath = "./source_data/course_info.json";//配置解析的文本路径
       JsonParser parser = new JsonParser();  //创建JSON解析器
       BufferedReader in = new BufferedReader(
           new InputStreamReader(new FileInputStream(dataPath), StandardCharsets.UTF_8),
@@ -40,18 +44,18 @@ public class connect {
 
       //新建解析csv的文件模块
       BufferedReader br = new BufferedReader(
-              new InputStreamReader(
-                      new FileInputStream("F://数据库原理//project//project_task2_1//src//select_course.csv")
-              )
+          new InputStreamReader(
+              new FileInputStream("./source_data/select_course.csv")
+          )
       );
 
       //程序化对数据库输入数据:  (int::n = 100只在测试阶段使用,后期换成json文件的长度)
       int n = jsonArray.size();
-      //insert_Course(jsonArray, conn, n);  //course表中数据的输入
-      //insert_Class(jsonArray, conn, n);   //class表中数据的输入
-      //insert_pre(jsonArray, conn, n);     //先修课输入 转换成数字及符号
-      //insert_teacher(jsonArray, conn, n); //teacher表中数据的输入
-      insert_student(br, conn, n);
+      // insert_Course(jsonArray, conn, n);  //course表中数据的输入
+      // insert_Class(jsonArray, conn, n);   //class表中数据的输入
+      // insert_pre(jsonArray, conn, n);     //先修课输入 转换成数字及符号
+      // insert_teacher(jsonArray, conn, n); //teacher表中数据的输入
+      // insert_student(br, conn, n);
 
     } catch (JsonIOException | FileNotFoundException | JsonSyntaxException e) {
       e.printStackTrace();
@@ -95,13 +99,15 @@ public class connect {
   }
 
   //teacher数据的输入封装
-  private static void insert_teacher(JsonArray jsonArray, Connection conn, int n) throws SQLException {
-    for(int i = 0; i < n; i++) {
+  private static void insert_teacher(JsonArray jsonArray, Connection conn, int n)
+      throws SQLException {
+    for (int i = 0; i < n; i++) {
       //构造查询，辅助teacher对象数据输入
       String sql_get_classId = "select id from class where className = ?";
       PreparedStatement ps_get_classId = conn.prepareStatement(sql_get_classId);
       //准备导入数据库所需的jdbc::PreparedStatement, 以及对应的sql语句
-      String sql_add_Teacher = "INSERT INTO teacher(id, classId, name) VALUES(DEFAULT,?,?)" + "ON conflict(classId) DO NOTHING;";
+      String sql_add_Teacher = "INSERT INTO teacher(id, classId, name) VALUES(DEFAULT,?,?)"
+          + "ON conflict(classId) DO NOTHING;";
       PreparedStatement ps_addTeacher = conn.prepareStatement(sql_add_Teacher);
       //从json文件中读取对应序号的信息，创造teacher对象，同时输入数据库中
       jsonToTeacher(jsonArray, ps_get_classId, i, ps_addTeacher);
@@ -117,7 +123,9 @@ public class connect {
       PreparedStatement ps_get_CourseId = conn.prepareStatement(sql_get_courseId);
       jsonToPre(jsonArray, ps_get_CourseId, i, prerequisite);
       //准备导入数据库所需的jdbc::PreparedStatement, 以及对应的sql语句
-      String sql_add_Pre = "INSERT INTO prerequisite(id, courseId, requisiteCourseId) VALUES(DEFAULT, ?, ?)" + "ON conflict(courseId)  DO NOTHING;";
+      String sql_add_Pre =
+          "INSERT INTO prerequisite(id, courseId, requisiteCourseId) VALUES(DEFAULT, ?, ?)"
+              + "ON conflict(courseId)  DO NOTHING;";
       PreparedStatement ps_add_Pre = conn.prepareStatement(sql_add_Pre);
       //将prerequisite对象中的信息导入数据库中（待完成）
       preToDatabase(ps_add_Pre, prerequisite);
@@ -125,13 +133,13 @@ public class connect {
   }
 
   //student数据的输入封装
-  private static void insert_student(BufferedReader br, Connection conn, int n) throws IOException, SQLException {
+  private static void insert_student(BufferedReader br, Connection conn, int n)
+      throws IOException, SQLException {
     //准备导入数据库所需的jdbc::PreparedStatement, 以及对应的sql语句
     String sql_add_student = "INSERT INTO student(id, name, sex, department, studentId) VALUES(DEFAULT,?,?,?,?)";
     PreparedStatement ps_addStudent = conn.prepareStatement(sql_add_student);
     String new_line;
-    while((new_line = br.readLine()) != null)
-    {
+    while ((new_line = br.readLine()) != null) {
       student student = new student();
       String[] info = new_line.split(",");
       student.setName(info[0].trim());
@@ -171,8 +179,9 @@ public class connect {
     class_info.setClassName(jsonOBJ_classArray.get("className").getAsString());
   }
 
-  private static void jsonToTeacher(JsonArray jsonArray, PreparedStatement ps_get_classId, int index, PreparedStatement ps_addTeacher)
-          throws SQLException {
+  private static void jsonToTeacher(JsonArray jsonArray, PreparedStatement ps_get_classId,
+      int index, PreparedStatement ps_addTeacher)
+      throws SQLException {
     //通过json找出需要查询的string:::course_name
     JsonObject jsonOBJ_classArray = jsonArray.get(index).getAsJsonObject();
     String class_Name = jsonOBJ_classArray.get("className").getAsString();
@@ -212,13 +221,12 @@ public class connect {
 
     // 检测先修课是否为空并且获取原始prerequisite字符串
     String pre;
-    if(jsonOBJ_Pre.get("prerequisite").equals(JsonNull.INSTANCE)) {
+    if (jsonOBJ_Pre.get("prerequisite").equals(JsonNull.INSTANCE)) {
       prerequisite.setPrerequisite_list(null);
       return;
     } else {
       pre = jsonOBJ_Pre.get("prerequisite").getAsString();
     }
-
 
     // 开始对字符产的转换处理模块
     // 以空格分割
@@ -267,19 +275,19 @@ public class connect {
     int t = 0;
     for (int i = 0; i < fin_arr.length; i++) {
       switch (new_arr[i]) {
-        case "(" :
+        case "(":
           fin_arr[t] = -3;
           break;
-        case ")" :
+        case ")":
           fin_arr[t] = -4;
           break;
-        case "或者" :
+        case "或者":
           fin_arr[t] = -1; // 或者 -> -1
           break;
-        case "并且" :
+        case "并且":
           fin_arr[t] = -2; // 并且 -> -2
           break;
-        default : {
+        default: {
 //                        if (new_arr[i].equals("化学原理")){
 //                            if (i + 1 < new_arr.length && (new_arr[i + 1].equals("A") || new_arr[i + 1].equals("B"))){
 //                                fin_arr[t] = "化学原理 " + new_arr[i + 1];
@@ -293,7 +301,8 @@ public class connect {
 //                        resultSet.next();
 //                        int course_Id = resultSet.getInt("id");
 //                        fin_arr[t] = course_Id + "";
-          if (i+1 < new_arr.length && new_arr[i + 1].length() == 1 && new_arr[i + 1].charAt(0) <= 90
+          if (i + 1 < new_arr.length && new_arr[i + 1].length() == 1
+              && new_arr[i + 1].charAt(0) <= 90
               && new_arr[i + 1].charAt(0) >= 65) {
             temp = new_arr[i] + " " + new_arr[i + 1];
             i++;
@@ -346,10 +355,11 @@ public class connect {
         case -4:
           while (true) {
             //数组越界
-            if(top >= 1)
+            if (top >= 1) {
               temp_int = stack[--top];
-            else
+            } else {
               break;
+            }
             if (temp_int == -3) {
               break;
             } else {
@@ -397,20 +407,22 @@ public class connect {
     ps_addClass.executeUpdate();
   }
 
-  private static void teacherToDatabase(PreparedStatement ps_addTeacher, teacher teacher) throws SQLException {
+  private static void teacherToDatabase(PreparedStatement ps_addTeacher, teacher teacher)
+      throws SQLException {
     ps_addTeacher.setInt(1, teacher.getClassId());
     ps_addTeacher.setString(2, teacher.getName());
     ps_addTeacher.executeUpdate();
   }
 
   public static void preToDatabase(PreparedStatement ps_addPre, prerequisite prerequisite)
-          throws SQLException {
+      throws SQLException {
     ps_addPre.setInt(1, prerequisite.getCourseId());
     ps_addPre.setString(2, prerequisite.getPrerequisite_list());
     ps_addPre.executeUpdate();
   }
 
-  public static void studentToDatabase(PreparedStatement ps_addTeacher, student student) throws SQLException {
+  public static void studentToDatabase(PreparedStatement ps_addTeacher, student student)
+      throws SQLException {
     ps_addTeacher.setString(1, student.getName());
     ps_addTeacher.setString(2, student.getSex());
     ps_addTeacher.setString(3, student.getDepartment());
