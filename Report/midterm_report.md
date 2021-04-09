fd75af06eb57af4dfeee9daca581396a8c19cc10
WHALEEYE <1094349973@qq.com>
2021-04-09 23:16:55 +0800

    2021/4/9 update
    See readme

---
+++ Report/midterm_report.md
@@ -0,0 +1,13 @@
+## CS307 Project Report (Midterm)
+
+### Part 1. Group Info and Contribution
+
+### Part 2. Task 1 Implementation & Introduction
+
+<img src="./table_design.png" alt="design diagram" style="zoom:100%;" />
+
+<center style="font-family:Arial;font-weight:bold">Figure 1. UML diagram of the tables</center>
+
+### Part 3. Task 2 Implementation & Analysis
+
+### Part 4. Task 3 Implementation & Analysis
--- source_data/postgresql_code.sql
+++ source_data/postgresql_code.sql
@@ -1,3 +1,4 @@
+--需要更新
 create table if not exists course
 (
     Id            serial primary key,
@@ -31,14 +32,6 @@ create table if not exists classList
     constraint fk_courseId foreign key (classId) references class (Id)
 );
 
-create table if not exists prerequisite
-(
-    Id                serial primary key,
-    courseId          integer unique,
-    requisiteCourseId varchar,
-    constraint fk_courseId foreign key (courseId) references course (Id)
-);
-
 create table if not exists teacher
 (
     Id      serial primary key,
@@ -47,14 +40,42 @@ create table if not exists teacher
     constraint fk_courseId foreign key (classId) references class (Id)
 );
 
+create table if not exists department
+(
+    id           serial primary key,
+    chinese_name varchar(20) unique not null,
+    english_name varchar(40) unique
+);
+
 create table if not exists student
 (
-    Id         serial primary key,
-    name       varchar,
-    sex        varchar,
-    department varchar,
-    studentId  int
+    id             serial primary key,
+    name           varchar(10) not null,
+    sex            char(1)     not null,
+    department_id  int,
+    student_number int         not null unique,
+    constraint dep_for_key foreign key (department_id) references department (id)
 );
+
+create table if not exists course_selection
+(
+    id         serial primary key,
+    student_id int not null,
+    course_id  int not null,
+    constraint stu_for_key foreign key (student_id) references student (id),
+    constraint cor_for_key foreign key (course_id) references course (id)
+);
+
+insert into department (chinese_name, english_name)
+values ('阿兹卡班', 'Azkaban');
+insert into department (chinese_name, english_name)
+values ('斯莱特林', 'Slytherin');
+insert into department (chinese_name, english_name)
+values ('赫奇帕奇', 'Hufflepuff');
+insert into department (chinese_name, english_name)
+values ('拉文克劳', 'Ravenclaw');
+insert into department (chinese_name, english_name)
+values ('格兰芬多', 'Gryffindor');
 
 delete
 from course;
