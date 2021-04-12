a9b64d50c5cef3a4927f9ca47ad8e746cf8e3705
WHALEEYE <1094349973@qq.com>
2021-04-12 10:47:43 +0800

    Part A finished
    Take a good sleep!

--- source_data/postgresql_code.sql
+++ source_data/postgresql_code.sql
@@ -1,41 +1,38 @@
---需要更新
 create table if not exists course
 (
-    Id            serial primary key,
-    courseId      varchar(10) unique not null,
-    courseCredit  int                not null,
-    totalCapacity int                not null,
-    courseHour    int                not null,
-    courseName    varchar            not null,
-    courseDept    varchar            not null,
-    constraint unique_course unique (courseName, courseDept)
+    Id                int primary key,
+    courseId          varchar(10) not null unique,
+    courseCredit      int         not null,
+    totalCapacity     int         not null,
+    courseHour        int         not null,
+    courseName        varchar     not null,
+    courseDept        varchar     not null,
+    requisiteCourseId varchar
 );
 
 create table if not exists class
 (
-    Id        serial primary key,
-    courseId  integer,
+    Id        int primary key,
+    courseId  int,
     className varchar not null,
-    teacher   varchar,
-
     constraint fk_courseId foreign key (courseId) references course (Id)
 );
 
 create table if not exists classList
 (
     Id        serial primary key,
-    classId   integer,
-    weekList  varchar[] not null,
+    classId   int,
+    weekList  varchar not null,
     location  varchar,
-    classTime varchar   not null,
-    weekDay   integer   not null,
+    classTime varchar not null,
+    weekDay   int     not null,
     constraint fk_courseId foreign key (classId) references class (Id)
 );
 
 create table if not exists teacher
 (
     Id      serial primary key,
-    classId integer unique,
+    classId int unique,
     name    varchar,
     constraint fk_courseId foreign key (classId) references class (Id)
 );
@@ -65,37 +62,3 @@ create table if not exists course_selection
     constraint stu_for_key foreign key (student_id) references student (id),
     constraint cor_for_key foreign key (course_id) references course (id)
 );
-
-insert into department (chinese_name, english_name)
-values ('阿兹卡班', 'Azkaban');
-insert into department (chinese_name, english_name)
-values ('斯莱特林', 'Slytherin');
-insert into department (chinese_name, english_name)
-values ('赫奇帕奇', 'Hufflepuff');
-insert into department (chinese_name, english_name)
-values ('拉文克劳', 'Ravenclaw');
-insert into department (chinese_name, english_name)
-values ('格兰芬多', 'Gryffindor');
-
-delete
-from course;
-delete
-from class;
-delete
-from prerequisite;
-TRUNCATE TABLE course RESTART IDENTITY CASCADE;
-TRUNCATE TABLE class RESTART IDENTITY CASCADE;
-TRUNCATE TABLE prerequisite RESTART IDENTITY CASCADE;
-
-select *
-from course
-where courseName = '毕业论文（设计）';
-alter table course
-    add constraint unique_course unique (courseName, courseDept);
-
-alter table course
-    drop constraint unique_course;
-
-select *
-from course
-where courseName like '%化学原理%';
